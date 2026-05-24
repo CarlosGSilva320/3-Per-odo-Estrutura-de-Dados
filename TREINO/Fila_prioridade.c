d862ae4f133dabe5c8bcbb52a58cb617d9e0c579
CarlosGSilva320 <car_gsilva@yahoo.com.br>
2026-05-24 03:14:26 -0300

    nova

---
+++ TREINO/Fila_prioridade.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+typedef struct {
+    int valor;
+    int prioridade;
+} Elemento;
+
+Elemento fila[MAX];
+int tamanho = 0;
+
+int filaVazia() {
+    return tamanho == 0;
+}
+
+int filaCheia() {
+    return tamanho == MAX;
+}
+
+void enqueue(int valor, int prioridade) {
+    if(filaCheia()) {
+        printf("Fila cheia\n");
+        return;
+    }
+
+    fila[tamanho].valor = valor;
+    fila[tamanho].prioridade = prioridade;
+    tamanho++;
+
+    printf("Elemento %d inserido com prioridade %d\n", valor, prioridade);
+}
+
+void dequeue() {
+    if(filaVazia()) {
+        printf("Fila vazia!");
+        return;
+    }
+
+    int maiorPrioridade = 0;
+
+    for (int i = 1; i < tamanho; i++) {
+        if (fila[i].prioridade > fila[maiorPrioridade].prioridade) {
+            maiorPrioridade = i;
+        }
+    }
+
+    printf("Removido: %d (Prioridade: %d)\n",
+    fila[maiorPrioridade].valor,
+    fila[maiorPrioridade].prioridade);
+
+    for (int i = maiorPrioridade; i < tamanho -1; i++) {
+        fila[i] = fila[i + 1];
+    }
+
+    tamanho--;
+}
+
+    void exibirFila() {
+        if (filaVazia()) {
+            printf("Fila vazia.");
+            return;
+        }
+
+        printf("\nFila de Prioridade:\n");
+
+        for(int i = 0; i < tamanho; i++) {
+            printf("Valor")
+        }
+    
+}
